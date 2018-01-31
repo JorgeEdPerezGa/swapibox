c757ab460d4a9aab06cbfcde7cc0f549c8b69aa4
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-01-30 19:41:00 -0700

    add people container

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,7 +7,7 @@ const Nav = () => {
     <header className = 'nav-header'>
       <NavLink className = 'nav nav-home' to = '/'>SWAPIBOX</NavLink>
       <div className = 'nav-menu'>
-        <p>PEOPLE</p>
+        <NavLink className = 'nav' to = '/people'>PEOPLE</NavLink>
         <p>PLANETS</p>
         <p>VEHICLES</p>
       </div>
--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Nav from '../Nav';
+import People from '../../containers/People';
 
 const Routes = () => {
   return (
     <div>
       <Route path = '/' component = { Nav }/>
+      <Route path = '/people' component = { People }/>
     </div>
   );
 };
