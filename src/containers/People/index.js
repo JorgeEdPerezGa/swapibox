745105cf4f25ed2832cade4ffc0280d36993cf1c
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-01-31 18:55:10 -0700

    refactor people container two

--- src/containers/People/index.js
+++ src/containers/People/index.js
@@ -15,45 +15,37 @@ class People extends Component {
     this.fetchPeople();
   }
 
-  handleState = async (url) => {
+  fetchPeople = () => {
+    const url = 'https://swapi.co/api/people/?page=1';
+    this.handleFetch(url);
+  }
+
+  handleFetch = async (url) => {
     const type = 'people';
     const people = await this.state.helper.fetchSwapi(url, type);
     this.setState({ people });
   }
 
-  fetchPeople = () => {
-    const url = 'https://swapi.co/api/people/?page=1';
-    this.handleState(url);
-  }
-
-  fetchNext = () => {
-    if (this.state.people.next) {
-      const url = this.state.people.next;
-      this.handleState(url);
-    }
-  }
-
-  fetchPrevious = async() => {
-    if (this.state.people.previous) {
-      const url = this.state.people.previous;
-      this.handleState(url);
-    }
-  }
-
   fetchLast = async () => {
     const url = 'https://swapi.co/api/people/?page=9';
-    this.handleState(url);
+    this.handleFetch(url);
   }
 
   render() {
     console.log(this.state.people);
+    const fetch = this.state.people;
+
     return (
       <div>
         <p>PEOPLE</p>
-        <button onClick = { () => this.fetchPeople() }>{'<<'}</button>
-        <button onClick = { () => this.fetchPrevious() }>{'<'}</button>
-        <button onClick = { () => this.fetchNext() }>{'>'}</button>
-        <button onClick = { () => this.fetchLast() }>{'>>'}</button>
+        <button
+          onClick = { () => this.fetchPeople() }>{'<<'}</button>
+        <button
+          onClick = { () => this.handleFetch(fetch.previous) }>{'<'}</button>
+        <button
+          onClick = { () => this.handleFetch(fetch.next) }>{'>'}</button>
+        <button
+          onClick = { () => this.fetchLast() }>{'>>'}</button>
       </div>
     );
   }
