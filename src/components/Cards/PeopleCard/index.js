81720f37f59aa5f6e230d4c01a6513d38fa84388
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-02-05 14:10:47 -0700

    add people card proptypes

--- src/components/Cards/PeopleCard/index.js
+++ src/components/Cards/PeopleCard/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FavoriteButton from '../../FavoriteButton';
+import PropTypes from 'prop-types';
 import './style.css';
 
 const PeopleCard = ({ card, name, birthYear, eyeColor, height, mass, skinColor, species, homeworld, homeworldPopulation, favorite, handleFavorites }) => {
@@ -21,6 +22,22 @@ const PeopleCard = ({ card, name, birthYear, eyeColor, height, mass, skinColor, 
       <p>homeworld population: { homeworldPopulation }</p>
     </article>
   );
+};
+
+PeopleCard.propTypes = {
+  handleFavorites: PropTypes.func,
+  card: PropTypes.object,
+  name: PropTypes.string,
+  birthYear: PropTypes.string,
+  eyeColor: PropTypes.string,
+  height: PropTypes.string,
+  residents: PropTypes.string,
+  mass: PropTypes.string,
+  skinColor: PropTypes.string,
+  species: PropTypes.string,
+  homeworld: PropTypes.string,
+  homeworldPopulation: PropTypes.string,
+  favorite: PropTypes.string
 };
 
 export default PeopleCard;
